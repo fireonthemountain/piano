78ffe0075890fe2e0bfe7a88ccc6b155572f25e7
fireonthemountain <cjsather@gmail.com>
2026-08-11 21:46:42 -0500

    Refresh cached piano app assets

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const SHELL='grand-piano-v1';
+const SHELL='grand-piano-v2';
 const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(SHELL).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==SHELL).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+  e.respondWith(fetch(e.request).then(res=>{
     if(res.ok){const copy=res.clone();caches.open(SHELL).then(c=>c.put(e.request,copy));}
     return res;
-  })));
+  }).catch(()=>caches.match(e.request)));
 });
